00df1ac2d44124ceb050420f5333b91e7077999f
Martin Tian <martintianmeng@gmail.com>
2018-10-16 10:14:07 -0400

    Update to openzeppelin-2.0 library

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.7;
 
-import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
-import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol';
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
 
-contract SimpleToken is StandardToken, DetailedERC20 {
+contract SimpleToken is ERC20, ERC20Detailed {
 
   constructor(string _name, string _symbol, uint8 _decimals)
-      DetailedERC20(_name, _symbol, _decimals)
-      public {}
+      ERC20Detailed(_name, _symbol, _decimals)
+      public {
+        _mint(msg.sender, 10000 * (10 ** uint256(_decimals)));
+      }
 }
