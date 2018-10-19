7ed1b2f4d7ad5d6dddbadc3242e3b23173ed4cf0
Martin Tian <martintianmeng@gmail.com>
2018-10-19 14:05:19 -0400

    rewrite token generation examples

--- migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-var SimpleToken = artifacts.require("./SimpleToken.sol");
-
-module.exports = function(deployer) {
-  const _name = "MyTestToken";
-  const _symbol = "MTT";
-  const _decimals = 18;
-
-  deployer.deploy(SimpleToken, _name, _symbol, _decimals);
-};
